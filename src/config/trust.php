7f48f830382e613ab3448ae8e54e3eace871839a
Alex House <house.alexander@gmail.com>
2015-02-14 16:53:08 +0000

    Add comments to trust config file

--- src/config/trust.php
+++ src/config/trust.php
@@ -1,21 +1,39 @@
 <?php
 
-
 return [
 
-    'models' => [
-        'user' => 'User',
-        'role' => 'Role',
-        'permission' => 'Permission',
-    ],
+  /*
+  |--------------------------------------------------------------------------
+  | Trust Models
+  |--------------------------------------------------------------------------
+  |
+  | In order for the traits to reference the correct models, specify them
+  | here (including full namespaces if applicable)
+  |
+  */
 
-    'tables' => [
-        'roles' => 'roles',
-        'user_role' => 'user_role',
-        'permissions' => 'permissions',
-        'permission_role' => 'permission_role',
-    ],
+  'models' => [
+    'user' => 'App\Models\User',
+    'role' => 'App\Models\Role',
+    'permission' => 'App\Models\Permission',
+  ],
 
+  /*
+  |--------------------------------------------------------------------------
+  | Trust Tables
+  |--------------------------------------------------------------------------
+  |
+  | The Trust migration and model traits will use the below table references
+  | when retrieving data
+  |
+  */
+
+  'tables' => [
+    'roles' => 'roles',
+    'user_role' => 'user_role',
+    'permissions' => 'permissions',
+    'permission_role' => 'permission_role',
+  ],
 
 ];
 
